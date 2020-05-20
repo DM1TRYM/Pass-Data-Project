f45d7bbfcd40a5e19582af5a9fbda690ce3d9ce8
Dmitrii <dm1trym2130@gmail.com>
2020-05-20 15:27:28 +0300

    Add files via upload

---
+++ Pass Data Project/Pass Data Project/SecondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Алексей Пархоменко on 20.02.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var login: String?
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let login = self.login else { return }
+        label.text = "Hello, \(login)!"
+    }
+    
+
+    @IBAction func goBackTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "unwindSegue", sender: nil)
+    }
+}
---
+++ Pass Data Project/Pass Data Project/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  PassDataProject
+//
+//  Created by Алексей Пархоменко on 20.02.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "detailSegue", sender: nil)
+    }
+    
+    @IBAction func unwindSegueToMainScreen(segue: UIStoryboardSegue) {
+        guard segue.identifier == "unwindSegue" else { return }
+        guard let svc = segue.source as? SecondViewController else { return }
+        self.resultLabel.text = svc.label.text
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? SecondViewController else { return }
+        dvc.login = loginTF.text
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
+
